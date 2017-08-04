bda53a4ef6f7f7c767ff9cd79f756bd3eb3bf5f0
Daniel Barde <mrbarde2k@gmail.com>
2017-08-04 23:29:01 +0100

    fix typo error in create reducer helper

--- src/CreateReducer.js
+++ src/CreateReducer.js
@@ -1,4 +1,4 @@
-modules.exports = function(initialState, handlers){
+module.exports = function(initialState, handlers){
     return function reducer(state = initialState, action) {
         if(handlers.hasOwnProperty(action.type)){
             return handlers[action.type](state, action);
