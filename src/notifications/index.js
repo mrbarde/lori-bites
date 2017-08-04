4bc6c08bcd49829fc1887e8352e202b9e3b54fcb
Daniel Barde <mrbarde2k@gmail.com>
2017-08-04 23:11:49 +0100

    change file extensions

--- src/notifications/index.js
+++ src/notifications/index.js
@@ -1,4 +1,4 @@
-const Notification = require('./components/Notifications.jsx');
+const Notification = require('./components/Notifications');
 const {lnRemove, lnPush} = require('./reducers');
 const {pushStart, removeStart} = require('./actions');
 
